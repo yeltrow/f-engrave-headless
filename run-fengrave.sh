8b9a2c0654bf8ad4e9f06a7ed27e543ba532685c
Matthew R Wortley <matthew_r_wortley@yahoo.com>
2022-12-21 21:43:39 +0000

    Updated the virtual environment wrapper file run-fengrave to source the irontools-venv.conf file to know where the virtual environment lives.

--- run-fengrave.sh
+++ run-fengrave.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+#
+# run-fengrave.sh: Runs the f-engrave python program in a virtual environment
+#                  specified in a file at ~/Documents/irontools/irontools-venv.conf
+#
+# The irontools-venv.conf sets the variable VENV_PATH to to the full path
+# to the virtual environment with something like this
+#
+# VENV_PATH=`realpath ~/Documents/pypy3-banana-venv` 
+#
+
+#
+#
 set -e
-source "/home/headless/Downloads/my-pypy3-env/bin/activate"
-python /home/headless/Downloads/f-engrave-headless/f-engrave.py "$@"
+CONF_PATH=`realpath ~/Documents/irontools/irontools-venv.conf`
+source $CONF_PATH
+echo VENV_PATH is ${VENV_PATH}
+source "${VENV_PATH}/bin/activate"
+python ~/Documents/f-engrave-headless/f-engrave.py "$@"
 
 
